a673bd82db07c7a73f454291abe34409a205af0c
John Saputo <jsaputo1@gmail.com>
2020-07-21 02:14:03 +0000

    added new store

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,19 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# Ask the user for a store name (store it in a variable)
+
+puts "Enter the store name"
+name = gets.chomp
+
+# Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+
+@new_store = Store.create(name: name)
+
+# Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+
+@new_store.errors.full_messages.each do |error|
+  puts error
+end
+
